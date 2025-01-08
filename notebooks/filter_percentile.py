757d7609e0148c517b80bbee4c2d54b9579eb641
Karthik Venkataramani <vkarthik@uw.edu>
2025-01-08 12:07:37 -0800

    clarify in comments working of percentile based filter

--- notebooks/filter_percentile.py
+++ notebooks/filter_percentile.py
@@ -2,8 +2,8 @@ import numpy as np
 import scipy
 
 # This method accepts a percentile threshold as part of pdal args, converts it to a Z-score
-# and then filters out points that fall outside this range. We use a nodata value (-9999) for 
-# points that fall outside of the specified threshold
+# Points that fall outside this range are classified as high noise (class 18), and are subsequently filtered out 
+# by PDAL. We use a nodata value (-9999) for points that fall outside of the specified threshold
 def filter_percentile(ins, outs):
     # pdal args is defined in the PDAL pipeline that calls this script
     percentile_threshold = pdalargs['percentile_threshold'] # type: ignore
